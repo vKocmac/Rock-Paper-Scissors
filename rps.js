a79fcbfba916be772540d571246d9eca343af5b6
vKocmac <vkocmac@gmail.com>
2022-09-28 21:06:00 +0300

    playRound function correction & game() creation

--- rps.js
+++ rps.js
@@ -2,7 +2,9 @@
 
 let playerSelection;
 let computerSelection;
-
+let playerWins = 0;
+let computerWins = 0;
+let draws = 0;
 
 function getComputerChoice(){
   let computerChoice;
@@ -24,22 +26,43 @@ function playRound(playerSelection,computerSelection){
     // console.log("You = " + playerSelection);
 
     if (playerSelection === computerSelection){
+      draws++;
       console.log("Draw");
     }else if (playerSelection === "rock"& computerSelection==="scissors") {
+      playerWins++;
       console.log("You win");
     }else if (playerSelection === "rock"& computerSelection==="paper") {
+      computerWins++;
       console.log("You lose");
     }else if (playerSelection === "paper"& computerSelection==="rock"){
+      playerWins++;
       console.log("You win");
     }else if (playerSelection === "paper"& computerSelection==="scissors"){
+      computerWins++;
       console.log("You lose");
     }else if (playerSelection === "scissors"& computerSelection==="paper"){
+      playerWins++;
       console.log("You win");
     }else if (playerSelection === "scissors"& computerSelection==="rock"){
+      computerWins++;
       console.log("You lose");
     }
+
     return(computerSelection,playerSelection);
   }
 
-getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+  for (let i=0; i<5; i++){
+    playRound(playerSelection,computerSelection);
+  }
+console.log("wins = " + playerWins + ", losses = " +computerWins + ", draws = " +draws);
+if (playerWins > computerWins){
+  console.log("Awesome!! You win!");
+}
+else if(playerWins > computerWins){
+  console.log("Shit happens...");
+}else{
+  console.log("Fair..it's a draw.");
+}
+}
+game();
